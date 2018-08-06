a4fb6f2e96b7850284dd470375fbcb29c0905e91
Gary <gary.houbre@epitech.eu>
2018-08-06 16:11:07 +0200

    Add Mailer Service

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,12 +12,55 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+	 * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return  [
+        ];
     }
+
+	/**
+	 * @param Request $request
+	 * @return JsonResponse
+	 *
+	 * @Route("/send/mail")
+	 */
+    public function sendEmailAction(Request $request)
+	{
+		// Configuration
+		$host 		= $request->get('host');
+		$username 	= $request->get('username');
+		$password 	= $request->get('password');
+		$port 		= $request->get('port');
+		$security 	= $request->get('security');
+
+		// Message
+		$to 		= $request->get('to');
+		//$toName 	= $request->get('toName');
+		$from		= $request->get('from');
+		//$fromName	= $request->get('fromName');
+		$subject	= $request->get('subject');
+		$body 		= $request->get('body');
+
+
+		// Initialise Configuration SwiftMailer without config.yml
+		$swift_transport = new \Swift_SmtpTransport($host, $port, $security);
+		$swift_transport->setUsername($username);
+		$swift_transport->setPassword($password);
+
+
+
+		$message = new \Swift_Message();
+		$message->setFrom($from);
+		$message->setTo($to);
+		$message->setBody($body);
+		$message->setSubject($subject);
+
+		$mailer = new \Swift_Mailer($swift_transport);
+		$mailer->send($message, $failed);
+
+		return new JsonResponse(['error' => $failed], JsonResponse::HTTP_ACCEPTED);
+	}
+
 }
